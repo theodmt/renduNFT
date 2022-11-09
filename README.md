e3a33067e081f07df2c4e6d3d0cc1087644f6940
theodmt <theo.dumont@edu.ece.fr>
2022-11-10 00:31:14 +0100

    renduNFT

---
+++ README.md
@@ -0,0 +1,38 @@
+# NFT Marketplace
+
+## Technology Stack & Tools
+
+- Solidity (Writing Smart Contract)
+- Javascript (React & Testing)
+- [Ethers](https://docs.ethers.io/v5/) (Blockchain Interaction)
+- [Hardhat](https://hardhat.org/) (Development Framework)
+- [Ipfs](https://ipfs.io/) (Metadata storage)
+- [React routers](https://v5.reactrouter.com/) (Navigational components)
+
+## Requirements For Initial Setup
+- Install [NodeJS](https://nodejs.org/en/), should work with any node version below 16.5.0
+- Install [Hardhat](https://hardhat.org/)
+
+## Setting Up
+### 1. Clone/Download the Repository
+
+### 2. Launch the app:
+```
+$ npm install
+$ npm run start
+```
+### 3. Main target of the app
+- First of all, you'll be able to connect to your metamask account.
+- Then, when you're connected, you have the possibility to see your Nfts, or create new ones.
+- To create your NFTs, click on the page create and follow the instructions. Then, the NFT will be available on the marketplace.
+- The app is also sustainable : the loading is not heavy because the design is minimalist
+
+
+### 4. Why is the blockchain important ?
+- To run this project, the blockchain is important because it is safer than the regular web marketplace. Every transaction is reported on the blockcain and it is easy to find them. The market of art can be a good example because the original NFT can be stored on a blockchain.
+- The blockchain can be useful for many projects. Indeed, it is a good way to keep, a picture for example, secure
+
+### 5. Group
+- Ilyas CHABI
+- Théo DUMONT
+- Julien FROIS
---
+++ src/frontend/components/Navbar.js
@@ -0,0 +1,42 @@
+import {
+    Link
+} from "react-router-dom";
+import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+
+
+const Navigation = ({ web3Handler, account }) => {
+    return (
+        <Navbar expand="lg" bg="secondary" variant="dark">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+                        <Nav.Link as={Link} to="/create">Créer</Nav.Link>
+                        <Nav.Link as={Link} to="/my-listed-items">Mes NFT</Nav.Link>
+                        <Nav.Link as={Link} to="/my-purchases">Mes achats</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        {account ? (
+                            <Nav.Link
+                                href={`https://etherscan.io/address/${account}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="button nav-button btn-sm mx-4">
+                                <Button variant="outline-light">
+                                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                                </Button>
+
+                            </Nav.Link>
+                        ) : (
+                            <Button onClick={web3Handler} variant="outline-light">Connect Wallet</Button>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+
+}
+
+export default Navigation;
